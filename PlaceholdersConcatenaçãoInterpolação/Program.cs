6bdada3292ffd4d57a42ded9e09eecd99e2055b0
Evandro2270 <evandro535@gmail.com>
2024-12-03 13:16:04 -0300

    Adicionar arquivos de projeto.

---
+++ PlaceholdersConcatenaçãoInterpolação/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+                char genero = 'F';
+                int idade = 32;
+                double saldo = 10.35784;
+                string nome = "Maria";
+
+                Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo); // placeholders
+                Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais"); //interpolação 
+                Console.WriteLine(nome + " tem " + idade + "anos e tem saldo igual a " + saldo.ToString("F2" ,CI) + "reais"); //concatenação 
+
+
+            }
+        }
+    }
+}
